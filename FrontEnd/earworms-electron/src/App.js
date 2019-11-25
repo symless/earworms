0b0f548f92e23c2c32bde5be2d09d534cfb98a09
Daun205 <daun205@hotmail.co.uk>
2019-11-25 14:44:48 +0000

    little bit of change -> trying to include ipcRenderer from electron in react

--- FrontEnd/earworms-electron/src/App.js
+++ FrontEnd/earworms-electron/src/App.js
@@ -1,16 +1,23 @@
 import React,{ Component} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+const electron = window.require('electron');
+const fs = electron.remote.require('fs');
+const ipcRenderer  = electron.ipcRenderer;
 
 class App extends Component{
   state = {}
   render(){
     return(
-      <div>
-        <div className = "IP address"> IP Address </div>
+      <div className = "loginMain">
+        <script>
+          window.require = require
+        </script>
+        <div className = "IP"> IP Address </div>
         <input type = "text"/>
 
-        <div className = "IP address"> Name </div>
+        <div className = "name"> Name </div>
         <input type = "text"/>
       </div>
     )
